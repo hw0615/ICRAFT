63f260433e6df329f07c1f00595c509cb544771f
CHOISHO <choisho1136@gmail.com>
2018-06-05 16:07:47 +0900

    [fix] hambuger btn
    토글시, 바디 스크롤 제거/생성

--- js/board-ajax.js
+++ js/board-ajax.js
@@ -1,4 +1,17 @@
 $(function () {
-
-
+  $.ajax({
+    
+    type: "GET",
+    url:"",
+    dataType: "JSON",
+    error: function(){
+      console.log('실패');
+    },
+    success: function(data){
+      console.log('data :', data);
+      for(var datas in data){
+        
+      }
+    }
+  })
 });
