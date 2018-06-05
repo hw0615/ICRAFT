e3e51ba4592972e19bc2b05651f80a6f721cedc2
jae.woo <jae.woo@blackrubystudio.com>
2018-06-05 21:35:40 +0900

    Add: cognito

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-northeast-2_Os4OJWBwz',
+        userPoolClientId: '2fg8saf7f6mt8raa8hcv3ptpdm',
+        region: 'ap-northeast'
+    },
+    api: {
+        invokeUrl: 'https://8e0rcemgq8.execute-api.ap-northeast-2.amazonaws.com/prod'
+    }
+};
