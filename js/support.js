0a0e9ab625769f9e0e127aa7a6780a051cb8b873
CHOISHO <choisho1136@gmail.com>
2018-06-18 17:49:09 +0900

    [fixing] support

--- js/support.js
+++ js/support.js
@@ -1,104 +1,146 @@
-function getData(){    
-    var supportTitle = document.getElementById("title").value;
-    var supportName = document.getElementById("name").value;
-    var supportEmail = document.getElementById("eMail").value;
-    var supportContent = document.getElementById("content").value;
-    var supportFile = document.getElementById("file");
-    // console.log('supportTitle :', supportTitle);
-    var object = new FormData();
-    // console.log('object :', object);
-   
-    object.append( "title", supportTitle);
-    object.append( "name", supportName);
-    object.append( "email", supportEmail);
-    object.append("message", supportContent);    
-    // object.append("filename" , supportFile.files[0]);
-    // console.log('object :', supportFile.files[0].name);
-    // var contentFile = new Blob(supportFile.files[0].file)
-    // console.log('contentFile :', contentFile);
-    // object.append("filename" , contentFile);
-    console.log('object :', object);
-    var box = {}
-    object.forEach(function(value, key){
-        box[key] = value;
+var data = {
+    "title": '',
+    "name": '',
+    "email": '',
+    "message": '',
+    "filename": ''
+};
+
+function getData(){      
+    data['title'] = document.getElementById("title").value;
+    data['name'] = document.getElementById("name").value;
+    data['email'] = document.getElementById("email").value;
+    data['message'] = document.getElementById("content").value;    
+    var supportFile = document.getElementById("file").files[0];              
+    var output = document.getElementById('output');          
+
+    function getBase64(supportFile) {
+        var reader = new FileReader();
+        // console.log('reader :', reader);   
+        reader.readAsDataURL(supportFile);                
+        reader.onload = function () {           
+            console.log('data :',  reader.result.split('base64,')[1]);                        
+            var newSpan = document.createElement("span"); 
+            // and give it some content 
+            var newContent = document.createTextNode(reader.result.split('base64,')[1]); 
+            // add the text node to the newly created div
+            newSpan.appendChild(newContent)   
+            output.appendChild(newSpan)               
+        };
+        // console.log('reader.onload  :', reader.onload() );
+        reader.onerror = function (error) {
+            console.log('Error: ', error);
+        };    
+    }   
+    getBase64(supportFile);
+
+    $('#output').bind('DOMNodeInserted DOMNodeRemoved', function() {
+       var aaa = document.querySelector("#output span").innerHTML;
+       data['filename'] = aaa;  
+    //    console.log('object :', data['filename']);     
+    //    console.log('data :', data);
+       getPost(data);    
     });
-    console.log('object :', box);    
-    getPost(box);
 }
 
-function getPost(box){
-    console.log('box :', box);
-    console.log('JSON.stringify(box) :', JSON.stringify(box));
+function getPost(data){         
+    console.log('data :', data);
+    console.log('data :', JSON.stringify(data));
     $.ajax({
         method: 'POST',
         url: _config.api.invokeUrl + '/contact',   
-        data: box,
-        // data: JSON.stringify(box),
+        // data: data,
+        data: JSON.stringify(data),
         contentType: 'application/json; charset=utf-8',
-        success: completePost,
-        error: function ajaxError(jqXHR, textStatus, errorThrown) {
-            console.error('Error requesting news: ', textStatus, ', Details: ', errorThrown);
-            console.error('Response: ', jqXHR.responseText);
-            alert('An error occured when requesting your news:\n' + jqXHR.responseText);
-        }
+        success: completePost
+        
     });                
 }
+
 function completePost() {    
-    console.log('box :', box);
-    window.location.href = 'index.html';
+    console.log('box :', data);
+    // window.location.href = 'index.html';
 }
 
 $(".get-btn").click(function(event){
     event.preventDefault();
-    getData()
+    getData()    
 })
 
 
-
-// $("#supportForm").submit(function(event){
-//     event.preventDefault(); //prevent default action 
-    
-//     var request_method = $(this).attr("method"); //get form GET/POST method
-//     var form_data = new FormData(this); //Creates new FormData object
-//     console.log('form_data :', form_data);
-//     $.ajax({
-//         url : _config.api.invokeUrl + '/contact',        
-//         method: request_method,
-//         enctype: "multipart/form-data",
-//         data : form_data,
-//         contentType: 'application/json; charset=utf-8',
-//         cache: false,
-//         processData:false
-//     }).done(function(response){ //
-//         console.log('response :', response);
-//     });
-// });
-// function fileUpload(event) {
-//     event.preventDefault(); //prevent default action 
-//     var request_method = $(this).attr("method"); //get form GET/POST method
-
-//     $('#supportForm').ajaxForm({
-//         url: _config.api.invokeUrl + '/contact',
-//         method: 'post',
-//         type: request_method,
-//         enctype: "multipart/form-data", // 여기에 url과 enctype은 꼭 지정해주어야 하는 부분이며 multipart로 지정해주지 않으면 controller로 파일을 보낼 수 없음
-//         success: function(result){        
-//           alert(result);
+// $("#supportForm").submit(function(e) {
+//     e.preventDefault();    
+//     var formData = new FormData(this);
+//     // var formData = $('"#supportForm').serialize();
+//     var box = {};
+//     var supportFile = document.getElementById("file").files[0];
+//     formData.forEach(function(value, key){
+//         if(key == "file"){
+//             key = "filename";
+//             value =  getBase64(supportFile);
 //         }
+//             box[key] = value;
+           
 
 //     });
+//     function getBase64(supportFile) {
+//         var reader = new FileReader();
+//         reader.readAsDataURL(supportFile);
+//         reader.onload = function () {           
+//           console.log('data :',  reader.result.split('base64,')[1]);
+//         };
+//         reader.onerror = function (error) {
+//           console.log('Error: ', error);
+//         };
+//         var filename = reader.result.split('base64,')[1]
+//         return filename
+//         //   console.log('data :', reader.result);
+//      }
+     
+    
 
-//     // 여기까지는 ajax와 같다. 하지만 아래의 submit명령을 추가하지 않으면 백날 실행해봤자 액션이 실행되지 않는다.
 
-//     $("#supportForm").submit();
-
-// }
-// var options = {
-//     url: _config.api.invokeUrl + '/contact',
-//     dataType:"text", //결과
-//     success:function(responseText){
-//         alert("업로드 성공!!");
-//     }, error:function(e){e.responseText();}
-// };
-
-// $("#supportForm").ajaxForm(options).submit();
+    
+//     console.log('box :', box);
+//     $.ajax({
+//         url: _config.api.invokeUrl + '/contact',     
+//         // method: 'post',    
+//         type: 'POST',
+//         headers: {
+//             'Access-Control-Allow-Origin': '*'
+//         },
+//         // data: box,
+//         // data: JSON.stringify(box),
+//         success: function (data) {
+//             alert(data)
+//         },
+//         cache: false,
+//         // contentType: false,
+//         contentType: 'application/json; charset=utf-8',
+//         processData: false,
+//         error: function ajaxError(jqXHR, textStatus, errorThrown) {
+//                     console.error('Error requesting news: ', textStatus, ', Details: ', errorThrown);
+//                     console.error('Response: ', jqXHR.responseText);
+//                     alert('An error occured when requesting your news:\n' + jqXHR.responseText);
+//                 }
+//     });
+//     // $.ajax({
+//     //     url: _config.api.invokeUrl + '/contact',   
+//     //     type: 'POST',
+//     //     data: box,
+//     //     // data: JSON.stringify(box),
+//     //     // headers: {
+//     //     //     'Access-Control-Allow-Origin': '*'
+//     //     // },
+//     //     cache: false,
+//     //     contentType: false,
+//     //     processData: false,
+//     //     // contentType: 'application/json; charset=utf-8',
+//     //     success: completePost,
+//     //     error: function ajaxError(jqXHR, textStatus, errorThrown) {
+//     //         console.error('Error requesting news: ', textStatus, ', Details: ', errorThrown);
+//     //         console.error('Response: ', jqXHR.responseText);
+//     //         alert('An error occured when requesting your news:\n' + jqXHR.responseText);
+//     //     }
+//     // });      
+// });
