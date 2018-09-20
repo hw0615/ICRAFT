837d8ca5a4e821c2d8aa50fdc1acea159dc062a7
Hyeonwoo Jeon <medianhw@gmail.com>
2018-09-20 11:53:43 +0900

    add Eng version files

---
+++ js/common_en.js
@@ -0,0 +1,121 @@
+$(function () {
+
+  // Solution Portfolio Nav에서 소분류 카테고리 hover시 나오는 메뉴 변수
+
+  // 소분류 카테고리 네비게이션
+  var nav = $('.small-menu');
+
+  // 스크롤 최상단올리기 이벤트 변수
+  var top = $('.move-top');
+
+  // Solution Portfolio 각 솔루션 네비게이션 변수
+  var nav1 = $('.nav-1');
+  var nav2 = $('.nav-2');
+  var nav3 = $('.nav-3');
+  var nav4 = $('.nav-4');
+  var nav5 = $('.nav-5');
+  var nav6 = $('.nav-6');
+  var nav7 = $('.nav-7');
+  var nav8 = $('.nav-8');
+  var nav9 = $('.nav-9');
+  var nav10 = $('.nav-10');
+  var nav11 = $('.nav-11');
+  var nav12 = $('.nav-12');
+
+  // Solution Portfolio & Main product 각 컨텐츠 변수
+  var sol1 = $('#sol1');
+  var sol2 = $('#sol2');
+  var sol3 = $('#sol3');
+  var sol4 = $('#sol4');
+  var sol5 = $('#sol5');
+  var sol6 = $('#sol6');
+  var sol7 = $('#sol7');
+  var sol8 = $('#sol8');
+  var sol9 = $('#sol9');
+  var sol10 = $('#sol10');
+  var sol11 = $('#sol11');
+  var sol12 = $('#sol12');
+
+  $(document).ready(function() {
+    // import header & footer
+    $('#header').load('header_en.html');
+    $('#footer').load('footer_en.html');
+
+    // 더 보기 이벤트 함수
+    $('.show-more').click(function (e) {
+      e.preventDefault();
+      $('div:hidden').slice(0,12).removeClass('hide')
+      
+      if($('div').hasClass('hide') === false) {
+        $('.more-wrap').addClass('hide')
+      }
+    })
+        
+  });
+
+  // 소분류 카테고리 스크롤 이벤트
+  $(window).scroll(function () {    
+    // if ( $(this).scrollTop() >= 540 ) {
+    if ( $(this).scrollTop() >= $('.location').offset().top - 90) {
+      nav.addClass('fixed-sub');
+      top.fadeIn();
+    } else {
+      nav.removeClass('fixed-sub');
+      top.fadeOut()
+    }
+  });
+
+  top.click(function() {
+    $( 'html, body' ).animate( { scrollTop : 0 }, 200);
+    return false;
+  });
+  nav1.click(function() {
+    $( 'html, body' ).animate( { scrollTop : sol1.offset().top }, 200);
+    return false;
+  })
+  nav2.click(function() {
+    $( 'html, body' ).animate( { scrollTop : sol2.offset().top }, 200);
+    return false;
+  })
+  nav3.click(function() {
+    $( 'html, body' ).animate( { scrollTop : sol3.offset().top }, 200);
+    return false;
+  })
+  nav4.click(function() {
+    $( 'html, body' ).animate( { scrollTop : sol4.offset().top }, 200);
+    return false;
+  })
+  nav5.click(function() {
+    $( 'html, body' ).animate( { scrollTop : sol5.offset().top }, 200);
+    return false;
+  })
+  nav6.click(function() {
+    $( 'html, body' ).animate( { scrollTop : sol6.offset().top }, 200);
+    return false;
+  })
+  nav7.click(function() {
+    $( 'html, body' ).animate( { scrollTop : sol7.offset().top }, 200);
+    return false;
+  })
+  nav8.click(function() {
+    $( 'html, body' ).animate( { scrollTop : sol8.offset().top }, 200);
+    return false;
+  })
+  nav9.click(function() {
+    $( 'html, body' ).animate( { scrollTop : sol9.offset().top }, 200);
+    return false;
+  })
+  nav10.click(function() {
+    $( 'html, body' ).animate( { scrollTop : sol10.offset().top }, 200);
+    return false;
+  })
+  nav11.click(function() {
+    $( 'html, body' ).animate( { scrollTop : sol11.offset().top }, 200);
+    return false;
+  })
+  nav12.click(function() {
+    $( 'html, body' ).animate( { scrollTop : sol12.offset().top }, 200);
+    return false;
+  })
+
+});
---
+++ js/news_en.js
@@ -0,0 +1,166 @@
+$(function () {
+
+  var index = Number(1)
+  // var pageUrl = window.location.href.split("?");    
+  // var pageNum = pageUrl[1].split("=")[1].replace(/[a-z,#,&,_]/g, "");     
+
+  $.ajax({    
+    type: "GET",
+    url: 'https://nllyo9o76k.execute-api.ap-northeast-2.amazonaws.com/prod/news?page=' + index,
+    dataType: "JSON",
+    error: function(){
+      console.log('실패');
+    },
+    beforeSend:function(){
+      $('.loading').removeClass('display-none');
+    },
+    complete:function(){
+      $('.loading').addClass('display-none');
+    },
+    success: function(data){ 
+
+      var dataResult = data.result
+
+      // 페이지네이션 목록 출력
+      var total = data.total  // 총 페이지 수
+      var pageCount = 10;
+      
+      function paging(total, pageCount, currentPage) {
+
+        console.log("currentPage-1 : " + currentPage);
+      
+        // var totalPage = Math.ceil(total/dataPerPage);    // 총 페이지 수
+        var totalPage = Math.ceil( total / 10 );    // 총 페이지 수
+        var pageGroup = Math.ceil( currentPage / pageCount );    // 페이지 그룹
+        
+        console.log("pageGroup-1 : " + pageGroup);
+        
+        var last = pageGroup * pageCount;    // 화면에 보여질 마지막 페이지 번호
+        if(last > totalPage)
+            last = totalPage;
+        // var first = last - ( last - 1 );    // 화면에 보여질 첫번째 페이지 번호
+        var first = last - (pageCount-1);     // 화면에 보여질 첫번째 페이지 번호
+        var next = last + 1;
+        var prev = first - 1;
+        
+        console.log("last-1 : " + last);
+        console.log("first-1 : " + first);
+        console.log("next-1 : " + next);
+        console.log("prev-1 : " + prev);
+
+        var html = "";
+
+        if(prev > 0)
+          html += "<li id='prev'> < </li> ";
+        
+        for( var i = first; i <= last; i++ ){
+          html += "<li class='page-num' id=" + i + ">" + i + "</li> ";
+        }
+        
+        if(last < totalPage)
+          html += "<li id='next'> > </li>";
+        
+        $(".pagination").html(html);    // 페이지 목록 생성
+        $(".pagination li#" + currentPage).css({
+          "text-decoration":"none", 
+          "color":"#FF8400", 
+          "font-weight":"bold",
+        });
+
+        // 페이지별 데이터 출력                               
+        $(".pagination li").click(function(){
+          var $item = $(this);
+          var $id = $item.attr("id");
+          var selectedPage = $item.text();
+          index = parseInt($(this).text())
+          console.log('index :', index);
+          
+          if($id == "next") {
+            selectedPage = next; 
+            index = parseInt(first) + 10
+          }
+          if($id == "prev") {
+            selectedPage = prev;
+            index = parseInt(last) - 10
+          }
+          $('.list').remove()
+
+          $.ajax({
+            url: 'https://nllyo9o76k.execute-api.ap-northeast-2.amazonaws.com/prod/news?page=' + index,
+            async: true,
+            type: "GET",
+            dataType: "json",
+            crossDomain: true,
+            success: function(data) {
+              var dataResult = data.result;
+              getData(dataResult)
+            },
+            beforeSend:function(){
+              $('.loading').removeClass('display-none');
+            },
+            complete:function(){
+              $('.loading').addClass('display-none');
+            },
+            error: function() {
+              console.log("failed");
+            }
+          }).responseText;
+          paging(total, pageCount, selectedPage);
+        });
+      }
+
+      function getData(dataResult) {
+        for (var j = 0; j < dataResult.length; j++) {
+          var el = dataResult[j];
+          var id = el.id;
+          var title = el.title;
+          var date = el.date;
+          var count = el.count;
+
+          var Tbody = $(".table tbody")
+          var newTr = Tbody.append(
+            "<tr class='list'>" + 
+            "<td>" + id + "</td>" + 
+            "<td>" + "<a href='news-post_en.html?page=" + id + "&id=" + id + "'>" + title + "</a>" + "</td>" + 
+            "<td>" + date + "</td>" +
+            "<td>" + count + "</td>" +
+            "</tr>"
+          )
+        }
+      }
+
+      // // DATA SUBTITLE
+      // for(var key in box.result[0]){
+      //   boardTitle.push(key)
+      // }              
+      // console.log('boardTitle :', boardTitle);
+      // // MAKE AND APPEND TR TD
+      // for(var j=0; j < box.result.length; j++){        
+      //   var trT = document.createElement("tr");         
+      //   for(var i=0; i < boardTitle.length; i++){
+      //     if(i!==2){ //body 제외
+      //       if(i==1){  
+      //         var aT = document.createElement("a");   
+      //         $(aT).attr("href", "news-post.html?page=" + pageNum  + "&id=" + box.result[j][boardTitle[0]]);
+      //         var tdT = document.createElement("td");                             
+      //         $(aT).append(box.result[j][boardTitle[i]]);        
+      //         $(tdT).append(aT);  
+      //         $(trT).append(tdT);                                
+      //       } else if(i==0 || i > 1){
+      //         var tdT = document.createElement("td");     
+      //         $(tdT).append(box.result[j][boardTitle[i]]);       
+      //         $(trT).append(tdT);             
+      //       } 
+      //     }
+      //   }      
+      //   $(board).append(trT);
+      // }
+
+      $("document").ready(function(){        
+        paging(total, pageCount, 1);
+        getData(dataResult)
+      });
+    }
+  })
+
+});
